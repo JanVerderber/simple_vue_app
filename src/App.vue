5044c92a86b9cd753b30f6ad01204c7fcf5c0df9
janverderber <jan.verderber@gmail.com>
2021-01-20 19:35:16 +0100

    Initial commit with merging project files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>  
+  </div>
+
+  <div v-if="showModal2">
+    <Modal theme="info" @close="toggleModal2">
+      <template v-slot:links>
+        <a href="#">latest news</a>
+        <a href="#">forum posts</a>
+      </template>
+      <h1>Information!</h1>
+      <p>Keep in touch with us!</p>
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">open modal</button>
+  <br>
+  <br>
+  <button @click="toggleModal2">open 2nd modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your ninja swag for half price!',
+      showModal: false,
+      showModal2: false
+    } 
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    },
+
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+
+    toggleModal2() {
+      this.showModal2 = !this.showModal2
+    }
   }
 }
 </script>
@@ -22,5 +70,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
